333fc879839958b14e0b5e3311dae6816c0b0b66
Jacob Godin <jacobgodin@gmail.com>
2009-03-15 22:35:23 -0300

    Added create class to enable easier game generation. Ported Game class to corrispond to this as well as the new grid system.

--- core/Game.java
+++ core/Game.java
@@ -1,13 +1,16 @@
 package core;
 import java.lang.reflect.Method;
+import java.util.ArrayList;
 import obj.*;
 import iface.*;
 import cmd.*;
+import game.*;
 
 public class Game {
    private Object cmd;
    private Parser gp;
    private Player p;
+   private ArrayList<Grid> map;
 
    public static void clearScreen() {
       for (int i = 0; i < 100; i++)
@@ -20,8 +23,9 @@ public class Game {
       // them to enter some info (name, age, etc)
       boolean getInfo = true;
       clearScreen();
-      Room startRoom = createRooms(); 
-      p = new Player(startRoom); 
+      this.map = Create.init();
+      Room startRoom = this.map.get(0).getStart();
+      p = new Player(startRoom);
       System.out.println("--- Welcome to Wormwood Version " + VERSION + " ---"); 
       // Print initial room description
       System.out.println(p.getRoom());
@@ -39,28 +43,6 @@ public class Game {
 
       if (this.cmd != null)
          executeCommand();
-   }
-
-   /** Creates all the rooms, returns starting room */
-   private Room createRooms () {
-      Command kate = new Kate();
-      Item gun = new Item("gun", "a frikkin gun", 3, kate);
-      Item[] items = {gun};
-      Room nRoom = new Room("North Forrest", "Northerly!");
-      Room eRoom = new Room("East Forrest", "Easterly!");
-      Room wRoom = new Room("West Forrest", "Westerly!");
-      Room sRoom = new Room("South Forrest", "Southerly!");
-      Room startRoom = new Room("Forrest Entrance", "A dark forrest.", items);
-      Exit sen = new Exit("s", startRoom, true, false, "empty");
-      Exit nes = new Exit("n", startRoom, false, false, "door");
-      Exit nExit = new Exit("n", nRoom, true, false, "empty");
-      Exit sExit = new Exit("s", sRoom, false, false, "empty");
-      startRoom.setExit(nExit);
-      startRoom.setExit(sExit);
-      nRoom.setExit(sen);
-      sRoom.setExit(nes);
-
-      return startRoom;
    }
 
    private void executeCommand () {
---
+++ game/Create.java
@@ -0,0 +1,50 @@
+/**
+ * 
+ * Used to create a game based on the wormwood engine
+ * 
+ */
+package game;
+import iface.Command;
+
+import java.util.ArrayList;
+
+import cmd.Kate;
+import obj.*;
+import core.*;
+import obj.*;
+
+public class Create {
+	/**
+	 * 
+	 * Used to create the game. The initial map ArrayList<Grid> must be used and returned.
+	 * The first grid added to the list is the starting grid, and the first room added to
+	 * this grid is the starting room.
+	 * 
+	 */
+	public static ArrayList<Grid> init(){
+		ArrayList<Grid> map = new ArrayList<Grid>();
+		Command kate = new Kate();
+	    Item gun = new Item("gun", "a frikkin gun", 3, kate);
+	    Item[] items = {gun};
+		Room r5 = new Room("North Forrest", "Northerly!");
+		Room r2 = new Room("East Forrest", "Easterly!");
+		Room r3 = new Room("West Forrest", "Westerly!");
+		Room r4 = new Room("South Forrest", "Southerly!");
+		Room r1 = new Room("Forrest Entrance", "A dark forrest.", items);
+		
+		Grid g1 = new Grid(20, 20);
+		g1.add(r1, 0, 0);
+		g1.add(r2, 1, 0);
+		g1.add(r3, 1, 1);
+		g1.add(r4, 4, 4);
+		g1.add(r5, 2, 0);
+		g1.linkRooms(r1, r2, true, false, "East-west gate");
+		g1.linkRooms(r2, r3, true, true, "Locked door");
+		g1.linkRooms(r2, r5, false, false, "Door blocked by large pile of cement blocks");
+		g1.linkRooms(r1, r4, true, true, "Impossible");
+		
+		map.add(g1);
+		
+		return map;
+	}
+}	
