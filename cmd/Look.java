0d8189657b0011cacbc96a43edd75d53ff881ff5
Allan Lavell <alavell@gmail.com>
2009-03-24 13:40:43 -0300

    Cleaned up the printing of exits. Now it prints with the rest of the paragraph. A period is also added to the end of the sentence.

--- cmd/Look.java
+++ cmd/Look.java
@@ -11,27 +11,31 @@ import core.handleData;
  * passed to it - generally the current room */
 public class Look implements Command {
    public void exec (Player p) {
-	  Room curRoom = p.getRoom();
-	  Grid curGrid = p.getGrid();
-	  String[] dirs = {"north", "east", "west", "south"};
+	   Room curRoom = p.getRoom();
+	   Grid curGrid = p.getGrid();
+	   String[] dirs = {"north", "east", "west", "south"};
 	 
-      Output.println(curRoom);
+      String d = "";
+
       
       for(int i = 0; i < dirs.length; i++){
-    	  Exit curExit = curGrid.getExit(curRoom, dirs[i].substring(0, 1));
-    	  if(curExit == null)
-    		  dirs[i] = null;
+    	   Exit curExit = curGrid.getExit(curRoom, dirs[i].substring(0, 1));
+    	   if(curExit == null)
+    	  	   dirs[i] = null;
       }
       
       String exits = handleData.arrToString(dirs);
       if(!exits.equalsIgnoreCase("")){
-    	  Output.print("There ");
-    	  if(exits.contains("and"))
-    	  	Output.print("are exits");
-    	  else
-    		  Output.print("is an exit");
-    	  Output.println(" to the "+exits);
+    	    d += " There ";
+    	   if(exits.contains("and"))
+    	      d += "are exits";
+    	   else
+    	      d += "is an exit";
+    	   d += " to the "+exits;
       }
+      d += ".";
+      
+      Output.println(curRoom + d);
    }
    
    public void construct(String params[]){}
