595c222e2d779321c295d17557458f5bb62888c8
Jacob Godin <jacobgodin@gmail.com>
2009-04-10 20:58:58 -0300

    Version 0.4

--- ww.java
+++ ww.java
@@ -5,7 +5,7 @@ import core.Game;
  
 public class ww {
   public static void main(String[] args) {
-      final String VERSION = "0.3";
+      final String VERSION = "0.4";
  
       Game g = new Game(VERSION);
  
